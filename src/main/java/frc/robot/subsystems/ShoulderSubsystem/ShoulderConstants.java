e8648313c226486abbeae169d6860bbf4de05fae
rayArjun2025 <rayankula.arjun@gmail.com>
2026-06-16 08:46:17 -0700

    Just added a ShoulderIOAutoLogged file

--- src/main/java/frc/robot/subsystems/ShoulderSubsystem/ShoulderConstants.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem/ShoulderConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.ShoulderSubsystem;
 
 public class ShoulderConstants {
+    public static final double MAX_ANGLE = Math.toRadians(270);
+    public static final double MIN_ANGLE = 0;
     
 }
--- src/main/java/frc/robot/subsystems/ShoulderSubsystem/ShoulderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem/ShoulderSubsystem.java
@@ -1,18 +1,21 @@
 package frc.robot.subsystems.ShoulderSubsystem;
 
+import edu.wpi.first.math.controller.PIDController;
 
 public class ShoulderSubsystem extends StateMachineSubsystemBase<ShoulderState>{
     private ShoulderIO io;
+    private PIDController pid;
 
     public ShoulderSubsystem(ShoulderIO io){
         super("Shoulder");
         this.io = io;
+        queueState(ShoulderState.IDLE);
+        pid = new PIDController(1, 0, 0);
     }
 
     @Override
     public void handleStateMachine() {
         switch(getState()){
-
             case INCREASE_SHOOTING_ANGLE:
                 break;
             case DECREASE_SHOOTING_ANGLE:
