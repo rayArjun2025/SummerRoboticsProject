44ebd5461324f720fa754eafb315d6f07e712a91
rayArjun2025 <rayankula.arjun@gmail.com>
2026-06-18 19:35:47 -0700

    Changed ShoulderConstants to git actual 7558 bot

--- src/main/java/frc/robot/subsystems/ShoulderSubsystem/ShoulderConstants.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem/ShoulderConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems.ShoulderSubsystem;
 
 public class ShoulderConstants {
-    public static final double MAX_ANGLE = Math.toRadians(270);
-    public static final double MIN_ANGLE = 0;
-    public static final double ZERO_REF = -Math.toRadians(90);
+    public static final double MAX_ANGLE = Math.toRadians(180);
+    public static final double MIN_ANGLE = -Math.toRadians(90);
+    public static final double ZERO_REF = Math.toRadians(90);
     public static final double GRAVITY_FF = 2.5;
     public static final double GEAR_RATIO = 150;
     public static final double ARM_LENGTH = 0.7366;
