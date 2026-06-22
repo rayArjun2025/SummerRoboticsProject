2b0d935d0f5016365484afb98ae863e5f7a84a6e
rayArjun2025 <rayankula.arjun@gmail.com>
2026-06-22 03:37:49 -0700

    Working on vision

--- src/main/java/frc/robot/subsystems/Vision/VisionIO.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionIO.java
@@ -1,6 +1,21 @@
 package frc.robot.subsystems.Vision;
 
-public interface VisionIO {
+import org.littletonrobotics.junction.AutoLog;
 
+import edu.wpi.first.math.geometry.Pose2d;
+
+public interface VisionIO {
+    @AutoLog
+    public static class VisionIOInputs{
+        public boolean hasTarget = false;
+        public int targetID  = -1;
+        public double tx = 0.0;
+        public double ty = 0.0;
+        public Pose2d robotPose = new Pose2d();
+    }
+
+    
+    public default void updateInputs(VisionIOInputs inputs) {}
+    public default void setPipeline(int mode) {}
     
 }
