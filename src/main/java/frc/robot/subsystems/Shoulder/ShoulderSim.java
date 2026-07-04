8652975f221a1dbeca467db23dee78fdc1e5f232
rayArjun2025 <rayankula.arjun@gmail.com>
2026-07-04 08:18:19 -0700

    Fixed sim w/ PIDController

--- src/main/java/frc/robot/subsystems/Shoulder/ShoulderSim.java
+++ src/main/java/frc/robot/subsystems/Shoulder/ShoulderSim.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.shoulder;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.robot.Constants;
@@ -8,19 +9,18 @@ import frc.robot.Constants;
 public class ShoulderSim implements ShoulderIO{
     private SingleJointedArmSim shoulderJoint;
     private double targetAngle_RAD;
+    private double motorVoltage;
+    private PIDController shoulderPID;
 
     public ShoulderSim(){
         double moi = SingleJointedArmSim.estimateMOI(ShoulderConstants.ARM_LENGTH, ShoulderConstants.ARM_MASS);
         shoulderJoint = new SingleJointedArmSim(DCMotor.getKrakenX60Foc(1), ShoulderConstants.GEAR_RATIO, moi, ShoulderConstants.ARM_LENGTH, ShoulderConstants.MIN_ANGLE, ShoulderConstants.MAX_ANGLE, true, ShoulderConstants.ZERO_REF);
+        shoulderPID = new PIDController(ShoulderConstants.KP, ShoulderConstants.KI, ShoulderConstants.KD);
     }
 
     @Override
     public void updateInputs(ShoulderIOInputs inputs){
-        double error = targetAngle_RAD - shoulderJoint.getAngleRads();
-        double motorVoltage = MathUtil.clamp(ShoulderConstants.KP * error, -12.0,12.0);
-        shoulderJoint.setInputVoltage(motorVoltage);
         shoulderJoint.update(Constants.globalDelta_s);
-
         inputs.angularVelocityRad = shoulderJoint.getVelocityRadPerSec();
         inputs.shoulderSwivelAngle_rad = shoulderJoint.getAngleRads();
         inputs.shoulderCurrent_amps = shoulderJoint.getCurrentDrawAmps();
@@ -31,6 +31,10 @@ public class ShoulderSim implements ShoulderIO{
     @Override
     public void setTargetAngle(double angle_rad){
         targetAngle_RAD = angle_rad;
+        double voltage = shoulderPID.calculate(shoulderJoint.getAngleRads(), targetAngle_RAD);
+        voltage = MathUtil.clamp(voltage, ShoulderConstants.LOW_CLAMP, ShoulderConstants.HIGH_CLAMP);
+        motorVoltage = voltage;
+        shoulderJoint.setInputVoltage(motorVoltage);
     }
 
     @Override
